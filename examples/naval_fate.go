45e08d29da8adf2dd214c48cc0085b6d275522d9
Keith Batten <kbatten@gmail.com>
2013-08-29 18:37:17 -0700

    add naval_fate example

---
+++ examples/naval_fate.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+        "fmt"
+        docopt "github.com/kbatten/docopt.go"
+)
+
+func main() {
+usage := `Naval Fate.
+
+Usage:
+  naval_fate.py ship new <name>...
+  naval_fate.py ship <name> move <x> <y> [--speed=<kn>]
+  naval_fate.py ship shoot <x> <y>
+  naval_fate.py mine (set|remove) <x> <y> [--moored|--drifting]
+  naval_fate.py -h | --help
+  naval_fate.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --speed=<kn>  Speed in knots [default: 10].
+  --moored      Moored (anchored) mine.
+  --drifting    Drifting mine.`
+
+    arguments := docopt.ParseLoud(usage, nil, true, "Naval Fate 2.0", false)
+    fmt.Println(arguments)
+}
