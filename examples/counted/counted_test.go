8d0f41e610b4ca70a1cea54aca6ffe50f7d24a2b
Waldemar Quevedo <wally@synadia.com>
2020-09-06 11:36:19 -0700

    Update examples
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- examples/counted/counted_test.go
+++ examples/counted/counted_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/docopt/docopt-go/examples"
+	"github.com/docopt/docopt.go/examples"
 )
 
 func Example() {
--- examples/unit_test/unit_test.go
+++ examples/unit_test/unit_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/docopt/docopt-go"
+	"github.com/docopt/docopt.go"
 	"reflect"
 	"testing"
 )
